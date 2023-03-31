5459e7cdc2e2b6432cb5a03395e93b28af32a100
BerkayMertt <berkay_hakan_gs@hotmail.com>
2023-03-31 20:43:57 +0300

    first Task Complated

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//var Hello = require("./my-module.js");
-import slugify from "slugify";
-import {hello} from "./my-module.js";
+import firstTask from "./my-module.js";
 
-//hello();
-
-console.log(slugify("Merhaba Dünya",))
+firstTask(1);
--- my-module.js
+++ my-module.js
@@ -1,3 +1,14 @@
-export function hello() {
-    console.log("Hello")
+import axios from "axios";
+
+async function firstTask(userId) {
+    //GetDatas
+    let userDatas = (await axios(`https://jsonplaceholder.typicode.com/users/${userId}`)).data
+    let userPosts = (await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)).data
+    //Filter Posts
+    userPosts = userPosts.filter(post => post.id === 1)
+    //Combine objects
+    let combinedObject = Object.assign({}, userDatas, { posts: userPosts })
+
+    console.log(combinedObject)
 }
+export default firstTask
